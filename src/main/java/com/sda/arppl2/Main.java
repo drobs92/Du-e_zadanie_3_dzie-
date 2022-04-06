8e0594664b60373aad2d4efcd9a866e368deeb42
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-06 19:47:40 +0200

    osoba
    osoba

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -3,5 +3,30 @@ package com.sda.arppl2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane!");
+
+
+        Osoba osoba1 = new Osoba("Marian", "Kowalski");
+
+
+
+        Osoba osoba2 = new Osoba("Adrian", "Roztocki");
+
+
+
+        Osoba osoba3 = new Osoba("Hugo", "Kołłątaj");
+
+
+
+        Osoba osoba4 = new Osoba("Alicja", "Wkrainieczarów");
+
+
+
+        Osoba osoba5 = new Osoba("Dagmara", "Drakula");
+
+
     }
+
+
+
+
 }
