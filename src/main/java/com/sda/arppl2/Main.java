620184c26a6a759d5ce5f868c5479d3ffc075653
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-07 19:06:08 +0200

    zmiana

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -5,33 +5,19 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane!");
- trello_red_1_wojtek
-
-
+        //osoby
         Osoba osoba1 = new Osoba("Marian", "Kowalski");
-
-
-
         Osoba osoba2 = new Osoba("Adrian", "Roztocki");
-
-
-
         Osoba osoba3 = new Osoba("Hugo", "Kołłątaj");
-
-
-
         Osoba osoba4 = new Osoba("Alicja", "Wkrainieczarów");
-
-
-
         Osoba osoba5 = new Osoba("Dagmara", "Drakula");
-=======
-        new Bear("Marian", 32);
-        new Bear("Olek", 2);
-        new Bear("Filipek", 10);
-        new Bear("Teddy", 3);
- master
+        ///miśki
+        Bear bear1 = new Bear("Marian", 32);
+        Bear bear2 = new Bear("Olek", 2);
+        Bear bear3 = new Bear("Filipek", 10);
+        Bear bear4 = new Bear("Teddy", 3);
 
 
     }
+}
 
